b0639f958418916aafbedcfccf09b084dbf21331
frpapu1920daw2@gmail.com <>
2019-09-26 18:39:15 +0200

    Versió 0.1 del projecte prjava01

--- src/main/java/prjava01/Main.java
+++ src/main/java/prjava01/Main.java
@@ -9,7 +9,7 @@ package prjava01;
  */
 import java.io.*;
 public class Main {
-    /**
+    /**     
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
